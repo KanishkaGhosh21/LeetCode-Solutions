1d0c1da66a04cfe13743b0f674c62a14ddd2c8c4
Kanishka Ghosh <91798916+KanishkaGhosh21@users.noreply.github.com>
2022-07-15 20:50:37 +0530

    Time: 6 ms (44.21%), Space: 6.3 MB (83.36%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.c
+++ 206-reverse-linked-list/206-reverse-linked-list.c
@@ -8,19 +8,34 @@
 
 
 struct ListNode* reverseList(struct ListNode* head){
+    // if(!head)
+    //     return head;
+    // struct ListNode* revList=(struct ListNode*)malloc(sizeof(struct ListNode));
+    // revList->val=head->val;
+    // revList->next=NULL;
+    // head=head->next;
+    // while(head)
+    // {
+    //     struct ListNode* newNode=(struct ListNode*)malloc(sizeof(struct ListNode));
+    //     newNode->val=head->val;
+    //     newNode->next=revList;
+    //     revList=newNode;
+    //     head=head->next;
+    // }
+    // return revList;
+    
+    
     if(!head)
         return head;
-    struct ListNode* revList=(struct ListNode*)malloc(sizeof(struct ListNode));
-    revList->val=head->val;
-    revList->next=NULL;
-    head=head->next;
-    while(head)
+    struct ListNode* ptr=head->next;
+    head->next=NULL;
+    while(ptr)
     {
-        struct ListNode* newNode=(struct ListNode*)malloc(sizeof(struct ListNode));
-        newNode->val=head->val;
-        newNode->next=revList;
-        revList=newNode;
-        head=head->next;
+        struct ListNode* next=ptr->next;
+        ptr->next=head;
+        head=ptr;
+        ptr=next;
     }
-    return revList;
+    return head;
+    
 }
