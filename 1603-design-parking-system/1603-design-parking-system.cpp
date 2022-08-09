ae4b2ae65a28385fd040dc6d6751c673e770fd62
Kanishka Ghosh <91798916+KanishkaGhosh21@users.noreply.github.com>
2022-08-09 15:22:11 +0530

    Time: 128 ms (12.56%), Space: 33.1 MB (72.42%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.cpp
+++ 1603-design-parking-system/1603-design-parking-system.cpp
@@ -1,29 +1,12 @@
 class ParkingSystem {
 public:
-    int big,medium,small;
+   vector<int> available;
     ParkingSystem(int big, int medium, int small) {
-        this->big=big;
-        this->medium=medium;
-        this->small=small;
+        available = {big, medium, small};
     }
-    
+
     bool addCar(int carType) {
-        if(carType==1 && big>0)
-        {
-            big--;
-            return true;
-        }
-        if(carType==2 && medium>0)
-        {
-            medium--;
-            return true;
-        }
-        if(carType==3 && small>0)
-        {
-            small--;
-            return true;
-        }
-        return false;
+        return available[carType - 1]-- > 0;
     }
 };
 
