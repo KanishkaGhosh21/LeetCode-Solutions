23eba1731384b47ce9ab2a67905168663064f485
Kanishka Ghosh <91798916+KanishkaGhosh21@users.noreply.github.com>
2022-09-12 23:01:50 +0530

    Time: 3 ms (55.02%), Space: 7.7 MB (77.67%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,21 +1,15 @@
 class Solution {
 public:
-    int findSum(vector<int> &nums,int i,vector<int> &dp)
-    {
-        if(i==0)
-            return nums[0];
-        if(i<0)
-            return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int pick=nums[i]+findSum(nums,i-2,dp);
-        int notPick=findSum(nums,i-1,dp);
-        return dp[i]=max(pick,notPick);
-    }
-    
     int rob(vector<int>& nums) {
+        if(nums.size()==1)
+            return nums[0];
         int maxSum=0;
-        vector<int> dp(nums.size()+1,-1);
-        return findSum(nums,nums.size()-1,dp);
+        int n=nums.size();
+        vector<int> dp(n);
+        dp[0]=nums[0];
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<n;i++)
+            dp[i]=max(dp[i-1],(dp[i-2]+nums[i]));
+        return dp[n-1];
     }
 };
